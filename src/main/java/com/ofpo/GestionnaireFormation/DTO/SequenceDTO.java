1abbedd622b7c860e9fdd3c0d69d3eb0afa5f0c4
Benoit <marliere.benoit59@gmail.com>
2025-05-23 10:34:28 +0200

    Sequence dto

--- src/main/java/com/ofpo/GestionnaireFormation/DTO/SequenceDTO.java
+++ src/main/java/com/ofpo/GestionnaireFormation/DTO/SequenceDTO.java
@@ -1,26 +1,21 @@
 package com.ofpo.GestionnaireFormation.DTO;
 
-public class ModuleDTO {
+public class SequenceDTO {
     private String libelle;
 
-    public ModuleDTO(String libelle){
-
+    public SequenceDTO(String libelle){
         this.libelle = libelle;
     }
 
-    public ModuleDTO(){
+    public SequenceDTO(){
 
     }
 
     public String getLibelle() {
-
-
         return libelle;
     }
 
     public void setLibelle(String libelle) {
-
-
         this.libelle = libelle;
     }
 }
